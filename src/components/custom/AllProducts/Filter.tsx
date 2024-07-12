37446d9c9ceb4b08914b6d801553dbcbaa2c8a07
mashuq0068 <mashuq0068@gmail.com>
2024-07-12 12:39:42 +0600

    adddded current filter

--- src/components/custom/AllProducts/Filter.tsx
+++ src/components/custom/AllProducts/Filter.tsx
@@ -1,12 +1,57 @@
-import { FaFilter } from "react-icons/fa6";
+import { FaFilter, FaStar } from "react-icons/fa6";
 import { IoSearch } from "react-icons/io5";
 import { FaFilterCircleXmark } from "react-icons/fa6";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { changeFilterState, clearFilter } from "@/redux/features/filter/filterSlice";
 
 const Filter = () => {
+  const currentFilter = useAppSelector((state) => state.filter);
+  const dispatch = useAppDispatch();
+  const { brand, category, price, rating } = currentFilter;
+  const handleBrand = (data: string) => {
+    const newFilter = {
+      ...currentFilter,
+      brand: data,
+    };
+    dispatch(changeFilterState(newFilter));
+  };
+  const handleCategory = (data: string) => {
+    const newFilter = {
+      ...currentFilter,
+      category: data,
+    };
+    dispatch(changeFilterState(newFilter));
+  };
+  const handleMaxPrice = (data: string) => {
+    const newFilter = {
+      ...currentFilter,
+      price: Number(data),
+    };
+    dispatch(changeFilterState(newFilter));
+  };
+  const handleRating = (data: string) => {
+    const newFilter = {
+      ...currentFilter,
+      rating: Number(data),
+    };
+    dispatch(changeFilterState(newFilter));
+  };
+  const handleClearFilter = () => {
+    
+    dispatch(clearFilter())
+  }
   return (
     <div className=" lg:mt-12 mt-5">
+      {/* search */}
       <div className="flex md:justify-normal justify-between gap-5">
-        <p className=" whitespace-nowrap text-xl flex gap-2 items-center font-medium">
+        <p  className=" whitespace-nowrap text-xl flex gap-2 items-center font-medium">
           Filter Products <FaFilter />
         </p>
         <form className="relative md:block hidden  w-full">
@@ -20,7 +65,7 @@ const Filter = () => {
           </button>
         </form>
         <div className=" whitespace-nowrap">
-          <button className="bg-black text-white px-4 py-2 flex gap-2 items-center">
+          <button onClick={handleClearFilter} className="bg-black text-white px-4 py-2 flex gap-2 items-center">
             Clear Filter <FaFilterCircleXmark className=" text-xl" />
           </button>
         </div>
@@ -30,7 +75,7 @@ const Filter = () => {
         <form className="relative md:hidden mt-5 block w-full">
           <input
             type="text"
-            className=" focus:outline-none  border px-4 py-2 border-slate-300 w-full"
+            className="  border px-4 py-2 border-slate-300 w-full"
             placeholder="Search by product name"
           />
           <button className=" bg-black text-white px-4 py-1 absolute right-0 h-full">
@@ -38,8 +83,131 @@ const Filter = () => {
           </button>
         </form>
       </div>
-
       {/* filters */}
+      <div className="mt-5 flex gap-5  justify-center flex-wrap">
+        {/* brand */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <button
+              className={`${
+                brand ? "bg-black text-white" : "bg-white text-black"
+              } relative  px-6 py-2 w  overflow-hidden border border-black   text-black shadow-2xl transition-all before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:duration-500 after:absolute after:right-0 after:top-0 after:h-full after:w-0 after:duration-500 hover:text-white hover:shadow-black hover:before:w-2/4 hover:before:bg-black hover:after:w-2/4 hover:after:bg-black uppercase`}
+            >
+              <span className="relative z-10 flex gap-2 items-center">
+                Brand
+              </span>
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="border border-slate-300 mt-3 w-max md:w-36">
+            <DropdownMenuRadioGroup value={brand} onValueChange={handleBrand}>
+              <DropdownMenuRadioItem value="puma">Puma</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="bottom">Nike</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="adidas">
+                Adidas
+              </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="reebok">
+                Reebok
+              </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="asics">
+                Asics{" "}
+              </DropdownMenuRadioItem>
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        {/* brand */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <button
+              className={`${
+                category ? "bg-black text-white" : "bg-white text-black"
+              } relative  px-6 py-2 w  overflow-hidden border border-black   text-black shadow-2xl transition-all before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:duration-500 after:absolute after:right-0 after:top-0 after:h-full after:w-0 after:duration-500 hover:text-white hover:shadow-black hover:before:w-2/4 hover:before:bg-black hover:after:w-2/4 hover:after:bg-black uppercase`}
+            >
+              <span className="relative z-10 flex gap-2 items-center">
+                Category
+              </span>
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="border border-slate-300 mt-3 w-max md:w-36">
+            <DropdownMenuRadioGroup
+              value={category}
+              onValueChange={handleCategory}
+            >
+              <DropdownMenuRadioItem value="golf">Golf</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="football">
+                Football
+              </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="cricket">
+                Cricket
+              </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="rugby">Rugby</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="tennis">
+                Tennis
+              </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="badminton">
+                Badminton
+              </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="basket ball">
+                Basket Ball{" "}
+              </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="baseball">
+                Baseball
+              </DropdownMenuRadioItem>
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        {/* brand */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <button
+              className={`${
+                price ? "bg-black text-white" : "bg-white text-black"
+              } relative  px-6 py-2 w  overflow-hidden border border-black   text-black shadow-2xl transition-all before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:duration-500 after:absolute after:right-0 after:top-0 after:h-full after:w-0 after:duration-500 hover:text-white hover:shadow-black hover:before:w-2/4 hover:before:bg-black hover:after:w-2/4 hover:after:bg-black uppercase`}
+            >
+              <span className="relative z-10 flex gap-2 items-center">
+                Max Price
+              </span>
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="border border-slate-300 mt-3 w-max md:w-36">
+            <DropdownMenuRadioGroup
+              value={price?.toString()}
+              onValueChange={handleMaxPrice}
+            >
+              <DropdownMenuRadioItem value="500">$500</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="400">$400</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="300">$300</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="200">$200</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="100">$100</DropdownMenuRadioItem>
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        {/* brand */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <button
+              className={`${
+                rating ? "bg-black text-white" : "bg-white text-black"
+              } relative  px-6 py-2 w  overflow-hidden border border-black   text-black shadow-2xl transition-all before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:duration-500 after:absolute after:right-0 after:top-0 after:h-full after:w-0 after:duration-500 hover:text-white hover:shadow-black hover:before:w-2/4 hover:before:bg-black hover:after:w-2/4 hover:after:bg-black uppercase`}
+            >
+              <span className="relative z-10 flex gap-2 items-center">
+                Rating
+              </span>
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="border w-max   border-slate-300 mt-3 ">
+            <DropdownMenuRadioGroup
+              value={rating?.toString()}
+              onValueChange={handleRating}
+            >
+              <DropdownMenuRadioItem className="flex gap-2 items-center" value="5">5 <FaStar className=" text-amber-500"/></DropdownMenuRadioItem>
+              <DropdownMenuRadioItem className="flex gap-2 items-center" value="4">4 <FaStar className=" text-amber-500"/></DropdownMenuRadioItem>
+              <DropdownMenuRadioItem className="flex gap-2 items-center" value="3">3 <FaStar className=" text-amber-500"/></DropdownMenuRadioItem>
+              <DropdownMenuRadioItem className="flex gap-2 items-center" value="2">2 <FaStar className=" text-amber-500"/></DropdownMenuRadioItem>
+              <DropdownMenuRadioItem className="flex gap-2 items-center" value="1">1 <FaStar className=" text-amber-500"/></DropdownMenuRadioItem>
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 };
---
+++ src/redux/features/filter/filterSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface IInitialState {
+  brand: string | undefined;
+  rating: number | undefined;
+  category: string | undefined;
+  price: number | undefined;
+}
+const initialState: IInitialState = {
+  brand: undefined,
+  rating: undefined,
+  category: undefined,
+  price: undefined,
+};
+const filterSlice = createSlice({
+  name: "filter",
+  initialState: initialState,
+  reducers: {
+    changeFilterState: (state, action: PayloadAction<IInitialState>) => {
+      return action.payload;
+    },
+    clearFilter: (state) => {
+      state.brand = undefined;
+      state.rating = undefined;
+      state.category = undefined;
+      state.price = undefined;
+    },
+  },
+});
+
+export const { changeFilterState, clearFilter } = filterSlice.actions;
+export default filterSlice.reducer;
